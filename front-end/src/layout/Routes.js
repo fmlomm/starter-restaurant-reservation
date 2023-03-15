bd204d29366d4b25cb1b1f48367df3d10228498b
Mel! <97633960+fmlomm@users.noreply.github.com>
2023-03-14 17:40:51 -0700

    fixed spelling errors in Routes.js

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -4,10 +4,10 @@ import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
 import useQuery from "../utils/useQuery";
-import ReservationCreate from "./reservations/ReservationCreate";
-import ReservationSeat from "./reservations/ReservationSeat";
-import TableCreate from "./tables/TableCreate";
-import ReservationEdit from "./reservations/ReservationEdit";
+import ReservationCreate from "./Reservations/ReservationCreate";
+import ReservationSeat from "./Reservations/ReservationSeat";
+import TableCreate from "./Tables/TableCreate";
+import ReservationEdit from "./Reservations/ReservationEdit";
 import ReservationSearch from "./ReservationSearch";
 
 /**
